794b5ecb6e7329972563c07f5848ece4e76f2082
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-05-09 16:37:57 -0500

    Tutorial 5 - Configuracion con anotaciones

--- src/main/java/com/mitocode/beans/Mundo.java
+++ src/main/java/com/mitocode/beans/Mundo.java
@@ -1,7 +1,10 @@
 package com.mitocode.beans;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Mundo {
 	
+	@Value("Hola Mundo!")
 	private String saludo;
 
 	public String getSaludo() {
--- src/main/java/com/mitocode/spring/App.java
+++ src/main/java/com/mitocode/spring/App.java
@@ -1,21 +1,32 @@
 package com.mitocode.spring;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.mitocode.beans.AppConfig;
+import com.mitocode.beans.AppConfig2;
 import com.mitocode.beans.Mundo;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/mitocode/xml/beans.xml");
-		Mundo m = (Mundo)appContext.getBean("mundo");
+		/*Con Xml
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/mitocode/xml/beans.xml");*/
+		
+		/*Pasando los Configuration como parametro*/
+		//ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class,AppConfig2.class);
+		
+		//Otra forma, utilizando register	
+		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+		appContext.register(AppConfig.class);
+		appContext.register(AppConfig2.class);
+		appContext.refresh();
+		
+		Mundo m = (Mundo)appContext.getBean("marte");
 		((ConfigurableApplicationContext)appContext).close();
 		
 		System.out.println(m.getSaludo());
-
 	}
 
 }

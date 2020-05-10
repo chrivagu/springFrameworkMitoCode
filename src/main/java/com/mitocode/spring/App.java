5b6e38e34c3668c324831ed97eb8638532a5865a
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-05-10 15:43:05 -0500

    Tutorial 6 - Inyeccion por constructor

--- src/main/java/com/mitocode/spring/App.java
+++ src/main/java/com/mitocode/spring/App.java
@@ -1,24 +1,22 @@
 package com.mitocode.spring;
 
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.mitocode.beans.AppConfig;
-import com.mitocode.beans.AppConfig2;
-import com.mitocode.beans.Mundo;
+import com.mitocode.beans.Persona;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		/*Con Xml
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/mitocode/xml/beans.xml");*/
+		/*Con Xml*/
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/mitocode/xml/beans.xml");
 		
 		/*Pasando los Configuration como parametro*/
 		//ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class,AppConfig2.class);
 		
 		//Otra forma, utilizando register	
-		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+		/*AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
 		appContext.register(AppConfig.class);
 		appContext.register(AppConfig2.class);
 		appContext.refresh();
@@ -26,7 +24,11 @@ public class App {
 		Mundo m = (Mundo)appContext.getBean("marte");
 		((ConfigurableApplicationContext)appContext).close();
 		
-		System.out.println(m.getSaludo());
+		System.out.println(m.getSaludo());*/
+		
+		Persona per = (Persona)appContext.getBean("persona");
+		
+		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo());
 	}
 
 }

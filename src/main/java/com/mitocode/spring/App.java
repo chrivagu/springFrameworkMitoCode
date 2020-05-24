d6786fea0564a125b3ca3c0947652b0ac4253cfa
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-05-23 21:37:01 -0500

    Tutorial 12 - Bean Scopes

--- src/main/java/com/mitocode/spring/App.java
+++ src/main/java/com/mitocode/spring/App.java
@@ -2,8 +2,9 @@ package com.mitocode.spring;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
-import com.mitocode.beans.Ciudad;
+//import com.mitocode.beans.Ciudad;
 import com.mitocode.beans.Persona;
 
 public class App {
@@ -31,9 +32,19 @@ public class App {
 		 */
 
 		Persona per = (Persona) appContext.getBean("persona");
+		Persona per2 = (Persona) appContext.getBean("persona");
+
+		per.setId(1);
+		per.setNombre("Jaimee");
+		per.setApodo("MitoCode");
 
 		System.out.println(per.getId() + " " + per.getNombre() + " " + per.getApodo() + " " + per.getPais().getNombre()
 				+ " " + per.getCiudad().getNombre());
+
+		System.out.println(per2.getId() + " " + per2.getNombre() + " " + per2.getApodo() + " "
+				+ per2.getPais().getNombre() + " " + per2.getCiudad().getNombre());
+
+		((ConfigurableApplicationContext) appContext).close();
 
 		/*
 		 * String nombresCiudades = "";

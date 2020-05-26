3d66ccb3e25f6549c58643ba4e770ec979cbb902
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-05-26 18:27:36 -0500

    Tutorial 15 - PostConstruct y PreDestroy

--- src/main/java/com/mitocode/beans/Persona.java
+++ src/main/java/com/mitocode/beans/Persona.java
@@ -1,5 +1,8 @@
 package com.mitocode.beans;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class Persona {
 	private int id;
 	private String nombre;
@@ -8,11 +11,13 @@ public class Persona {
 	private Ciudad ciudad;
 
 	// Metodo para ejecutarse antes de la inicializacion
+	@PostConstruct
 	private void initBean() {
 		System.out.println("Antes de inicializar el bean");
 	}
 
 	// Metodo para ejecutarse justo antes de ser destruido
+	@PreDestroy
 	private void destroyBean() {
 		System.out.println("Bean a punto de ser destruido");
 	}

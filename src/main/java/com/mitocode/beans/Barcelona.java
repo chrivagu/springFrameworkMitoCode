119000d818bf9e41943d053f110ecdd3cfef2035
Christian Valencia Gutierrez <Christian.X.Valencia.-ND@disney.com>
2020-05-28 09:28:40 -0500

    Tutorial 18 - Uso de interfaces

---
+++ src/main/java/com/mitocode/beans/Barcelona.java
@@ -0,0 +1,11 @@
+package com.mitocode.beans;
+
+import com.mitocode.interfaces.IEquipo;
+
+public class Barcelona implements IEquipo {
+
+	public String mostrar() {
+		return "Barcelona FC";
+	}
+
+}
--- src/main/java/com/mitocode/spring/App.java
+++ src/main/java/com/mitocode/spring/App.java
@@ -1,24 +1,21 @@
 package com.mitocode.spring;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.mitocode.beans.Ciudad;
-import com.mitocode.beans.Persona;
+import com.mitocode.interfaces.IEquipo;
 
 public class App {
 
 	public static void main(String[] args) {
 
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/mitocode/xml/beans.xml");
-
-		Persona per = (Persona) appContext.getBean("persona");
-
-		Ciudad ciu = (Ciudad) appContext.getBean("ciudad");
-
-		System.out.println(per.getApodo());
-		System.out.println(ciu.getNombre());
+		// Jugador jug = (Jugador) appContext.getBean("messi");
+		IEquipo bar = (IEquipo) appContext.getBean("barcelona");
+		// System.out.println(jug.getNombre() + "-" +
+		// jug.getEquipo().mostrar());
+		System.out.println(bar.mostrar());
 
 		((ConfigurableApplicationContext) appContext).close();
 	}
